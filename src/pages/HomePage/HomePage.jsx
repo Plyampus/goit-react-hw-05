2e2c1b145f221c4128a95d8529d9b684b02ddbd9
pavlo <pavlohvozdetskyi@protonmail.com>
2024-06-04 22:14:18 +0300

    Update Mentor Request

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,20 +1,21 @@
-import { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom"; // Додано useLocation
-import css from "./HomePage.module.css";
-import { fetchTrendMovies } from "../../theMovieApi";
-import toast from "react-hot-toast";
-import Loader from "../../components/Loader/Loader";
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom'; // Додано useLocation
+import css from './HomePage.module.css';
+import { fetchTrendMovies } from '../../theMovieApi';
+import toast from 'react-hot-toast';
+import Loader from '../../components/Loader/Loader';
+import MovieList from '../../components/MovieList/MovieList';
 
 const notify = () =>
-  toast.error("Something went wrong. Please, try again!", {
+  toast.error('Something went wrong. Please, try again!', {
     style: {
-      border: "1px solid #000000",
-      padding: "16px",
-      color: "#000000",
+      border: '1px solid #000000',
+      padding: '16px',
+      color: '#000000',
     },
     iconTheme: {
-      primary: "#000000",
-      secondary: "#f5f5f5",
+      primary: '#000000',
+      secondary: '#f5f5f5',
     },
   });
 
@@ -43,16 +44,8 @@ export default function HomePage() {
     <div className="container">
       <div className={css.homePage}>
         <h1>Trending Today</h1>
-        <ul>
-          {movieData.map((item) => (
-            <li key={item.id}>
-              <Link to={`/movies/${item.id}`} state={{ from: location }}>
-                {item.title}
-              </Link>
-            </li>
-          ))}
-          {loading && <Loader />}
-        </ul>
+        <MovieList movies={movieData} location={location} />
+        {loading && <Loader />}
       </div>
     </div>
   );
--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,48 +1,48 @@
-import { useState, useEffect } from "react";
-import css from './MoviePage.module.css'
-import { useLocation, useSearchParams } from "react-router-dom";
-import { fetchMovieByKeyword } from "../../theMovieApi";
-import SearchBar from "../../components/SearchBar/SearchBar";
-import MovieList from "../../components/MovieList/MovieList";
-import Loader from "../../components/Loader/Loader";
-import toast from "react-hot-toast";
+import { useState, useEffect } from 'react';
+import css from './MoviePage.module.css';
+import { useLocation, useSearchParams } from 'react-router-dom';
+import { fetchMovieByKeyword } from '../../theMovieApi';
+import SearchBar from '../../components/SearchBar/SearchBar';
+import MovieList from '../../components/MovieList/MovieList';
+import Loader from '../../components/Loader/Loader';
+import toast from 'react-hot-toast';
 
-const notify = (msg) =>
+const notify = msg =>
   toast.error(`${msg}`, {
     style: {
-      border: "1px solid #000000",
-      padding: "16px",
-      color: "#000000",
+      border: '1px solid #000000',
+      padding: '16px',
+      color: '#000000',
     },
     iconTheme: {
-      primary: "#000000",
-      secondary: "#f5f5f5",
+      primary: '#000000',
+      secondary: '#f5f5f5',
     },
   });
 
 export default function MoviesPage() {
   const location = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
-  const movieName = searchParams.get("movieName") ?? "";
+  const movieName = searchParams.get('movieName') ?? '';
   const [moviesList, setMoviesList] = useState([]);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    if (movieName === "") {
-      notify("Please, enter the keyword!");
+    if (movieName === '') {
+      notify('Please, enter the keyword!');
       return;
     }
     setMoviesList([]);
     setLoading(true);
-    const getMovieByKeyword = async (movieName) => {
+    const getMovieByKeyword = async movieName => {
       try {
-        await fetchMovieByKeyword(movieName).then((data) => {
+        await fetchMovieByKeyword(movieName).then(data => {
           if (!data.results.length) {
             setLoading(false);
             setError(true);
             return console.log(
-              "There is no movies with this request. Please, try again"
+              'There is no movies with this request. Please, try again'
             );
           }
           setError(false);
@@ -50,14 +50,14 @@ export default function MoviesPage() {
           setLoading(false);
         });
       } catch (error) {
-        notify("Something went wrong. Please, try again!");
+        notify('Something went wrong. Please, try again!');
         console.log(error);
       }
     };
     getMovieByKeyword(movieName);
   }, [movieName]);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
     const searchForm = e.currentTarget;
     setSearchParams({ movieName: searchForm.elements.movieName.value });
@@ -71,7 +71,9 @@ export default function MoviesPage() {
         {error && (
           <p>There is no movies with this request. Please, try again</p>
         )}
-        <MovieList movies={moviesList} location={location} />
+        {moviesList.length > 0 && (
+          <MovieList movies={moviesList} location={location} />
+        )}
         {loading && <Loader />}
       </div>
     </main>
